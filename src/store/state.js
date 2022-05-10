235d225f106fb4406ce90ed2a472c874e1c95edb
dorinebertonTlse <dorineberton@gmail.com>
2022-05-10 19:50:09 +0200

    change url

--- src/store/state.js
+++ src/store/state.js
@@ -5,9 +5,10 @@ export default () => {
     token: null,
     authenticated: false,
     users: [],
-    // urlBase: 'https://back-securiface.herokuapp.com/',
-    urlWS: 'http://localhost:5500/',
-    urlBase: 'http://localhost:5000/',
+    urlBase: 'https://back-securiface.herokuapp.com/',
+    urlWS: 'https://socket-securiface.herokuapp.com/',
+    // urlWS: 'http://localhost:5500/',
+    // urlBase: 'http://localhost:5000/',
     connection: ''
   }
 }
