1b8461abee1b40a1f1c2edb65b6d9faff7e1ab39
dorinebertonTlse <dorineberton@gmail.com>
2022-06-05 19:49:26 +0200

    add video

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,10 +1,24 @@
 <template>
   <div v-if="$vuetify.breakpoint.mdAndUp">
-      <h2>Caméras</h2>
-    </div>
-    <div v-else>
-      <h3>Caméras</h3>
-    </div>
+      <h2>Caméra</h2>
+      <video autoplay :width="$vuetify.breakpoint.lgAndUp ? 1000 : 900" style="margin-top: 12px">
+        <source
+          src="@/assets/video.mp4"
+          type="video/mp4"
+        >
+        <p>Désolé, votre navigateur ne supporte pas ce type de vidéo.</p>
+      </video>
+  </div>
+  <div v-else>
+    <h3>Caméra</h3>
+    <video autoplay :width="$vuetify.breakpoint.xs ? 300 : 500" style="margin-top: 36px">
+      <source
+        src="@/assets/video.mp4"
+        type="video/mp4"
+      >
+      <p>Désolé, votre navigateur ne supporte pas ce type de vidéo.</p>
+    </video>
+  </div>
 </template>
 <script>
 export default {
